decd954c65e688b921c5f1db557b9e733cf87487
daiananiedd <115374844+daiananiedd@users.noreply.github.com>
2023-02-12 15:27:11 -0300

    a

---
+++ exercicios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}KM por hora.`)
+if(vel>60){ //condição simples, pois não tem else (senão)
+    console.log(`Você ultrapassou a velocidade permitida! MULTADO.`)
+}
+console.log(`Dirija sempre usando seu cinto de segurança`)
