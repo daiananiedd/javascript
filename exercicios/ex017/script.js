857b23fc732b65dac29e9b04ef6ad38888873b5d
daiananiedd <115374844+daiananiedd@users.noreply.github.com>
2023-02-19 11:09:18 -0300

    a

---
+++ exercicios/ex017/script.js
@@ -0,0 +1,19 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 90){
+   
+}
+
+ /*var c = 1
+ while (c <= 6){
+     console.log(`Passo ${c}`)
+     c++
+ }
+
+
+
+ 
+só não faça ctrl c cntrl v, pelo amor da sua dignidade!!!
+*/
